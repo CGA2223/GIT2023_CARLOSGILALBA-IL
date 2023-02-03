5c0dfe0f676f142482720c00ba59ad4fa9e25be5
CGA2223 <a_carlos.gil.albanil@iespablopicasso.es>
2023-02-03 13:27:37 +0100

    commit1

---
+++ vehiculo.java
@@ -0,0 +1,12 @@
+public class vehiculo {
+    private String marca;
+    private String modelo;
+    private String matricula;
+    public vehiculo(String marca, String modelo, String matricula) {
+      this.marca = marca;
+      this.modelo = modelo;
+      this.matricula = matricula;
+      
+    }
+  
+  }
